f5e0664cb643ae8c4a22cc2ea8a121ad03ce1e49
SaraM09 <sayeramusfika@pursuit.org>
2024-01-26 19:46:55 -0500

    Sara's 03-22 exercise almost there

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import UserProfile from './components.js/UserProfile';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+
+
+      
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Link to='/user/sam'>Sam's Profile</Link>
+      <br />
+      <Link to='/user/Mick'>Mick's Profile</Link>
+      <br />
+      <Link to='/user/Alice'>Alice's Profile</Link>
+      <br />
+      <Link to='/user/Steve'>Steve's Profile</Link>
+      <Routes>
+        <Route path='/user/:username' element={<UserProfile />} />
+      </Routes>
+    </Router>
   );
 }
+
+
+
 
 export default App;
